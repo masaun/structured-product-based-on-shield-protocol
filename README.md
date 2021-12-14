6a6fcffb24fdedc2c684eca9514841ad87a9e715
masaun <masanoriuno@gmail.com>
2021-12-14 09:24:17 -0500

    [Deployment]: Define the 00_deploy_StructuredProduct.js in ./scripts/deployment

--- README.md
+++ README.md
@@ -4,6 +4,34 @@ https://gitcoin.co/issue/shielddaodev/shield-bounties/1/100027177
 
 <br>
 
+## 【Deployment】
+- Deployment on local network
+```
+npm run deploy-local:StructuredProduct
+```
+( `$ npx hardhat run scripts/deployment/00_deploy_StructuredProduct.js --network hardhat` )
+
+<br>
+
+- Deployment on BSC testnet
+```
+npm run deploy-bsc_testnet:StructuredProduct
+```
+( `$ npx hardhat run scripts/deployment/00_deploy_StructuredProduct.js --network bsc_testnet` )
+
+
+<br>
+
+## 【Test】
+```
+npm run test:Scenario
+```
+( `$ npx hardhat test ./test/scenario.test.js --network hardhat` )
+
+<br>
+
+
+
 <hr>
 
 # Basic Sample Hardhat Project
---
+++ scripts/deployment/00_deploy_StructuredProduct.js
@@ -0,0 +1,24 @@
+const { ethers } = require("hardhat")
+
+
+/**
+ * @dev - This is a deployment file for the StructuredProduct.sol
+ * Network: BSC testnet
+ */
+async function main() {
+
+    const _sldDAIContract = "0x7125E80fF9c5ae3073bf30769d0963B6d0B57515"  // @dev - Assign the SLDDAIContract.sol deployed
+    const _privateDAIPool = "0xaDea523A790b34CF6D0989a803262b2A38FD7cdA"  // @dev - Assign the PrivateDAIPool.sol deployed 
+    const _publicDAIPool = "0xb3F55De3a56F15d6C94cF74753F52e447519E563"   // @dev - Assign the PublicDAIPool.sol deployed
+
+    const StructuredProduct = await ethers.getContractFactory("StructuredProduct")
+    const structuredProduct = await StructuredProduct.deploy(_sldDAIContract, _privateDAIPool, _publicDAIPool)
+    console.log("StructuredProduct deployed to:", structuredProduct.address)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
