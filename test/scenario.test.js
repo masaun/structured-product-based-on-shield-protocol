cf68c3c55d24957273556510d00cbcf24331203f
masaun <masanoriuno@gmail.com>
2021-12-14 09:36:40 -0500

    [.editorconfig]: Add .editorconfig

---
+++ test/scenario.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Scenario test", function () {
+
+    it("Should ~", async function () {
+        // [Todo]:
+    })
+
+})
