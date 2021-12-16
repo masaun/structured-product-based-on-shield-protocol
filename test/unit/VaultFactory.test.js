545769a48ed236d3a916a65e3fc56a533ac301f5
masaun <masanoriuno@gmail.com>
2021-12-16 10:34:40 -0500

    [Test]: Deploy the VaultFactory.sol in unit test

--- test/unit/VaultFactory.test.js
+++ test/unit/VaultFactory.test.js
@@ -3,7 +3,20 @@ const { ethers } = require("hardhat");
 
 describe("Unit test of the VaultFactory.sol", function () {
 
-    it("Should ~", async function () {
+    //@dev - Deployed-contract instance
+    let vaultFactory
+
+    //@dev - Deployed-addresses
+    let VAULT_FACTORY
+
+    it("Deploy the VaultFactory.sol", async function () {
+        const VaultFactory = await ethers.getContractFactory("VaultFactory")
+        vaultFactory = await VaultFactory.deploy()
+        VAULT_FACTORY = vaultFactory.address
+        console.log(`Deployed-address of the VaultFactory.sol: ${ VAULT_FACTORY }`)
+    })
+
+    it("Should issue a new vault", async function () {
         // [Todo]:
     })
 
