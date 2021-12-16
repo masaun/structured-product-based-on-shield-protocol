70cbb30b0a5201bfd2eea06db4d7d153aab1da7e
masaun <masanoriuno@gmail.com>
2021-12-16 09:51:48 -0500

    [SC]: Remove 'HUNDRED_PERCENT' from the VaultType enum

--- contracts/vault-commons/VaultEnums.sol
+++ contracts/vault-commons/VaultEnums.sol
@@ -8,15 +8,15 @@ pragma solidity ^0.6.12;
 contract VaultEnums {
 
     /**
-     * @dev - Vaults type:
+     * @dev - Vaults type (Product description):
      *  a. 100% capital protected at maturity with an interest guarantee, customize the margin and APY
      *  b. 100% capital protected at maturity without interest guarantee, customize the margin and APY is 0
      *  c. non capital protected at maturity with limited loss, customize max loss ratio
      *  d. non capital protected at maturity without max loss, customize 100% loss percentage
      */
     enum VaultType { 
-        HUNDRED_PERCENT_CAPITAL_PROTECTED_AT_MATURITY_WITH_AN_INTEREST_GUARANTEE,
-        HUNDRED_PERCENT_CAPITAL_PROTECTED_AT_MATURITY_WITHOUT_AN_INTEREST_GUARANTEE,
+        CAPITAL_PROTECTED_AT_MATURITY_WITH_AN_INTEREST_GUARANTEE,
+        CAPITAL_PROTECTED_AT_MATURITY_WITHOUT_AN_INTEREST_GUARANTEE,
         NON_CAPITAL_PROTECTED_AT_MATURITY_WITH_LIMITED_LOSS,
         NON_CAPITAL_PROTECTED_AT_MATURITY_WITHOUT_MAX_LOSS
     }
