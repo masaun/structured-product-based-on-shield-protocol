ab903338c4fe7f0fa1d9971c2a49c80923dbe4c5
masaun <masanoriuno@gmail.com>
2021-12-16 14:12:10 -0500

    [Test]: Define the VaultFactory.test.js

--- test/unit/Vault.test.js
+++ test/unit/Vault.test.js
@@ -1,7 +1,60 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
+//@dev - ethers.js related methods
+const { toWei, fromWei, getEventLog, getCurrentBlock, getCurrentTimestamp } = require('../ethersjs-helper/ethersjsHelper')
+
 
 describe("Unit test of the Vault.sol", function () {
+
+    //@dev - Signer of each accounts
+    let signerOfIssuer
+    let signerOfUser
+
+    //@dev - Deployed-contract instance
+    let vaultFactory
+
+    //@dev - Deployed-addresses
+    let VAULT_FACTORY
+    let VAULT_1
+    let VAULT_2
+    let VAULT_3
+
+    it("Get signatures of each accounts", async function () {
+        [signerOfIssuer, signerOfUser] = await ethers.getSigners()
+        console.log(`Signer of a issuer: ${ JSON.stringify(signerOfIssuer) }`)
+        console.log(`Signer of a user: ${ JSON.stringify(signerOfUser) }`)
+    })
+
+    it("Deploy the VaultFactory.sol", async function () {
+        const VaultFactory = await ethers.getContractFactory("VaultFactory")
+        vaultFactory = await VaultFactory.deploy()
+        VAULT_FACTORY = vaultFactory.address
+        console.log(`Deployed-address of the VaultFactory.sol: ${ VAULT_FACTORY }`)
+
+        expect(VAULT_FACTORY).to.equal(vaultFactory.address);
+    })
+
+    it("Should issue a new vault", async function () {
+        const transaction = await vaultFactory.connect(signerOfIssuer).issueVault()
+        const txReceipt = await transaction.wait()
+        //const _txReceipt = JSON.stringify(txReceipt, null, 2)
+
+        //@dev - Check event log of "VaultCreated"
+        const EVENT_NAME = "VaultCreated"
+        const eventLog = await getEventLog(txReceipt, EVENT_NAME)
+        console.log(`event log of "VaultCreated": ${ eventLog }`)
+
+        const ISSUER = eventLog[0]
+        const VAULT_CREATED = eventLog[1]
+        console.log(`"issuer" that is retrieved from event log of "VaultCreated": ${ ISSUER }`)
+        console.log(`"vault address" that is retrieved from event log of "VaultCreated": ${ VAULT_CREATED }`)
+
+        expect(ISSUER).to.equal(signerOfIssuer.address)
+
+        //@dev - Assign a new vault created into variable of Vault No.1
+        VAULT_1 = VAULT_CREATED
+        console.log(`Vault No.1:  ${ VAULT_1  }`)
+    })
 
 })
