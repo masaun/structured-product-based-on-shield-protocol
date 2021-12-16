58cab2c48076b54e9c4dd882b76ce66a0bc0b91f
masaun <masanoriuno@gmail.com>
2021-12-16 10:48:24 -0500

    [Test]: Execute 'Get signature of each accounts' and issueVault() in the VaultFactory.test.js

--- test/unit/VaultFactory.test.js
+++ test/unit/VaultFactory.test.js
@@ -3,21 +3,34 @@ const { ethers } = require("hardhat");
 
 describe("Unit test of the VaultFactory.sol", function () {
 
+    //@dev - Sign of each accounts
+    let issuerSign
+    let userSign
+
     //@dev - Deployed-contract instance
     let vaultFactory
 
     //@dev - Deployed-addresses
     let VAULT_FACTORY
 
+    it("Get signatures of each accounts", async function () {
+        [issuerSign, userSign] = await ethers.getSigners()
+        console.log(`Signature of a issuer: ${ JSON.stringify(issuerSign) }`)
+        console.log(`Signature of a user: ${ JSON.stringify(userSign) }`)
+    })
+
     it("Deploy the VaultFactory.sol", async function () {
         const VaultFactory = await ethers.getContractFactory("VaultFactory")
         vaultFactory = await VaultFactory.deploy()
         VAULT_FACTORY = vaultFactory.address
         console.log(`Deployed-address of the VaultFactory.sol: ${ VAULT_FACTORY }`)
+
+        expect(VAULT_FACTORY).to.equal(vaultFactory.address);
     })
 
     it("Should issue a new vault", async function () {
-        // [Todo]:
+        const transaction = await vaultFactory.connect(issuerSign).issueVault()
+        const txReceipt = transaction.wait()
     })
 
 })
